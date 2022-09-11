758eb37ab8e7447d2e5baf35536a7486e3796b57
PinkInk <tim.pelling@gmail.com>
2022-09-11 11:58:55 +0100

    Rename demo

--- demo.ps1
+++ demo.ps1
@@ -2,11 +2,19 @@ import-module .\QuickForms.psd1
 
 $demo = New-QuickForm -Title "Demo Form" -LabelWidth 200 -ControlWidth 400
 
-$myFirstName = $demo.AddRow('TextBox', 'First Name:', { Write-Host $this.Text })
-$MySurname = $demo.AddRow('TextBox', 'Surname:', { Write-Host $this.Text })
+$myFirstName = $demo.AddRow('TextBox', 'First Name:', { $MyUserID.Text = "$($MyFirstName.Text[0]).$($MySurname.Text)" })
+
+$MySurname = $demo.AddRow('TextBox', 'Surname:', { $MyUserID.Text = "$($MyFirstName.Text[0]).$($MySurname.Text)" })
+
+$MyUserID = $demo.AddRow('TextBox', 'User ID:')
+$MyUserID.Enabled = $false
+
 $MyPassword = $demo.AddRow('PasswordBox', 'Password:')
+
 $MyConfirmPassword = $demo.AddRow('PasswordBox', 'Confirm Password:', { Write-Host "$($MyPassword.Text -eq $this.Text)" })
+
 $MySex = $demo.AddRow("CheckBox", 'Male', { Write-Host $this.Checked })
+
 $MyOptions = $demo.AddRow("ComboBox", "Sex:", @("Male", "Female"), { Write-Host $this.SelectedItem })
 
 $demo.AddAction({ 
