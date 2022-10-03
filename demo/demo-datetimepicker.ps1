7488c2d3c56395a2dbc810c1120a7b9c84bf54c5
PinkInk <tim.pelling@gmail.com>
2022-10-03 18:40:41 +0100

    first issue

---
+++ demo/demo-datetimepicker.ps1
@@ -0,0 +1,25 @@
+import-module ..\QuickForms.psd1
+
+$demo = New-QuickForm -Title "Various DateTimePickers" -LabelWidth 100 -ControlWidth 450
+
+Add-Title -Form $demo | Out-Null
+Add-Title -Form $demo -Label 'Add-DateTimePicker -Form $demo -Label "Date" -Type Date' -Bold | Out-Null
+$Date = Add-DateTimePicker -Form $demo -Label "Date" -Type Date
+
+Add-Title -Form $demo | Out-Null
+Add-Title -Form $demo -Label 'Add-DateTimePicker -Form $demo -Label "Time" -Type Time' -Bold | Out-Null
+$Time = Add-DateTimePicker -Form $demo -Label "Time" -Type Time
+
+Add-Title -Form $demo | Out-Null
+Add-Title -Form $demo -Label 'Add-DateTimePicker -Form $demo -Label "DateTime" -Type DateTime' -Bold | Out-Null
+$DateTime = Add-DateTimePicker -Form $demo -Label "DateTime" -Type DateTime
+
+Add-Title -Form $demo | Out-Null
+Add-Title -Form $demo -Label 'Add-DateTimePicker -Form $demo -Label "Set" -Type Date' -Bold | Out-Null
+Add-Title -Form $demo -Label '                   -DateTime (Get-Date -Year 1999 -Month 12 -Day 1)' -Bold | Out-Null
+$Set = Add-DateTimePicker -Form $demo -Label "Set" -Type Date -DateTime (Get-Date -Year 1999 -Month 12 -Day 1)
+
+Add-Title -Form $demo | Out-Null
+Add-Action -Form $demo
+
+$demo.Show()
